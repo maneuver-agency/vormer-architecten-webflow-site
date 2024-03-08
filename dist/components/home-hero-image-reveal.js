2f0d5682f9e0620ad5ebc38ca603ef3345a60845
Paras Shah <parasshah195@gmail.com>
2024-03-08 21:51:33 +0530

    Build

---
+++ dist/components/home-hero-image-reveal.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{function t(){window.gsap.fromTo("[data-image-reveal-parent] > div",{opacity:1},{opacity:0,duration:3/50,stagger:{each:3/50,from:"random"}})}})();
---
+++ dist/components/home-projects-load.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{window.fsAttributes=window.fsAttributes||[];window.fsAttributes.push(["cmsload",t=>{let[s]=t;s.on("renderitems",o=>{console.log(o)})}]);})();
