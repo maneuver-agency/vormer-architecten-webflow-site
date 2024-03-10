5726aad8235e88307f46f41211b7548a588f46dc
Paras Shah <parasshah195@gmail.com>
2024-03-10 14:09:25 +0530

    Build

--- dist/components/home-hero-image-reveal.js
+++ dist/components/home-hero-image-reveal.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{function t(){window.gsap.fromTo("[data-image-reveal-parent] > div",{opacity:1},{opacity:0,duration:3/50,stagger:{each:3/50,from:"random"}})}})();
+"use strict";(()=>{function n(){let o="[data-image-reveal-parent] > div",a=10*5;window.gsap.fromTo(o,{opacity:1},{opacity:0,duration:.3,stagger:{each:1/a,from:"random"}})}})();
