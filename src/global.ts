1418145964130054a3de5ef93eebe8b2be3075b2
Paras Shah <parasshah195@gmail.com>
2024-03-10 13:59:32 +0530

    Add custom cursor on the site

--- src/global.ts
+++ src/global.ts
@@ -2,10 +2,12 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { animatedDetailsAccordions } from './components/accordions';
+import { cursorInit } from './components/cursor';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow?.push(() => {
   animatedDetailsAccordions();
+  cursorInit();
 });
